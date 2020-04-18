fbace2d5cd9934a3695ac447d6fbe3cb4602dcdd
megane <megane.londono@hotmail.com>
2020-04-18 12:21:57 +0200

    unknown separated from area to manipulate separately

--- script.js
+++ script.js
@@ -4,3 +4,34 @@ import {
 } from "./modules/settings";
 
 const form = document.querySelector("form");
+// to access form from everywhere: 
+window.form = form;
+const elements = form.elements;
+// to access form elements from everywhere: 
+window.elements = elements;
+
+// CONTROLLING ELEMENTS
+// disable "area" when "unknown" is clicked
+elements.unknown.addEventListener("click", e => {
+    elements.area.disabled = !elements.area.disabled;
+});
+
+// FORM RELATED EVENTS
+// when a key is pressed, set value to what is being pressed
+elements.area.addEventListener("keyup", e => {
+    console.dir(e.target.value);
+})
+
+// submit
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    console.log("submitted")
+})
+
+/* // JS POWERED VALIDATION
+const formIsValid = form.checkValidity();
+if (formIsValid) {
+    console.log("yay!")
+} else {
+    console.log("not ready for submit")
+} */
