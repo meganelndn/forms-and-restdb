667ffbc58438768e836c7606b64187d1a2187f5c
megane <megane.londono@hotmail.com>
2020-04-18 18:40:37 +0200

    advanced validation with js done on all input fields

--- script.js
+++ script.js
@@ -4,34 +4,64 @@ import {
 } from "./modules/settings";
 
 const form = document.querySelector("form");
-// to access form from everywhere: 
+//to access form from everywhere: 
 window.form = form;
 const elements = form.elements;
-// to access form elements from everywhere: 
+//to access elements from everywhere: 
 window.elements = elements;
 
-// CONTROLLING ELEMENTS
-// disable "area" when "unknown" is clicked
+
+// HOW TO VALIDATE CORRECTLY
+/* 
+1. remove all error msgs
+2. loop through
+3. show custom error msgs 
+    - create with JS
+    - have them in the DOM hide/show
+    - "post" data
+*/
+
+
+//disable "area" when "unknown" is clicked
 elements.unknown.addEventListener("click", e => {
     elements.area.disabled = !elements.area.disabled;
 });
 
-// FORM RELATED EVENTS
-// when a key is pressed, set value to what is being pressed
-elements.area.addEventListener("keyup", e => {
-    console.dir(e.target.value);
-})
+//delete default HTML error messages
+form.setAttribute("novalidate", true);
 
 // submit
 form.addEventListener("submit", e => {
     e.preventDefault();
-    console.log("submitted")
-})
+    let validForm = true;
+    // REMOVE VALIDATION WHEN SUBMITTING
+    const formElements = form.querySelectorAll("input");
+    formElements.forEach(el => {
+        el.classList.remove("invalid");
+    });
 
-// JS POWERED VALIDATION
-/* const formIsValid = form.checkValidity();
-if (formIsValid) {
-    console.log("yay!")
-} else {
-    console.log("not ready for submit")
-}  */
+    // converted cbs into an array, bc otherwise "cbs.filter" is not a function
+    const cbs = [...form.querySelectorAll(`[name=cities]`)];
+    const checked = cbs.filter(el => el.checked)
+
+    const errorContainerCities = form.querySelector("fieldset p");
+
+    if (checked.length == 0) {
+        validForm = false;
+        errorContainerCities.classList.remove("hidden");
+    } else {
+        errorContainerCities.classList.add("hidden");
+    }
+
+    if (form.checkValidity() && validForm) {
+        //send to restdb/api
+        console.log("submit ready");
+        // ADDING VALIDATION IN CASE OF ERROR
+    } else {
+        formElements.forEach(el => {
+            if (!el.checkValidity()) {
+                el.classList.add("invalid");
+            }
+        });
+    }
+});
